120e04005c42e8f83d8bcec621e9b50cb7f92811
Biswo <biswajitdash907@gmail.com>
2024-02-29 09:31:14 +0530

    handle message

--- src/home.js
+++ src/home.js
@@ -1,27 +1,24 @@
 import React, { useEffect } from 'react';
-import { useLocation } from 'react-router-dom'; // Import useLocation hook
+import { useLocation } from 'react-router-dom';
 
 function Home() {
-    const location = useLocation(); // Use useLocation hook to get the current location
+    const location = useLocation();
 
     useEffect(() => {
         const loadRazorpay = async () => {
-
             const urlParams = new URLSearchParams(window.location.search);
             const company = urlParams.get('company');
             const user = urlParams.get('user');
             const amount = urlParams.get('amount');
 
-            // Load Razorpay script dynamically
             const script = document.createElement('script');
             script.src = 'https://checkout.razorpay.com/v1/checkout.js';
             script.async = true;
             document.body.appendChild(script);
             script.onload = () => {
-                // Initialize Razorpay when script is loaded
                 const rzp = new window.Razorpay({
                     key: 'rzp_test_NEFkyKlmiaaw6s',
-                    amount: amount ? amount : 50000, // Example: 500 INR
+                    amount: amount ? amount : 50000,
                     currency: 'INR',
                     name: company ? company : 'Your Company Name',
                     description: 'Product or service description',
@@ -30,19 +27,23 @@ function Home() {
                         contact: '9999999999',
                     },
                     handler: function (response) {
-                        // Send payment success message to parent window
-                        window.ReactNativeWebView.postMessage('paymentSuccess');
+                        // Check if payment was successful
+                        if (response.razorpay_payment_id) {
+                            // Payment successful
+                            window.ReactNativeWebView.postMessage('paymentSuccess');
+                        } else {
+                            // Payment failed or cancelled
+                            window.ReactNativeWebView.postMessage('paymentFailed');
+                        }
                     },
                 });
 
-                // Open Razorpay checkout immediately
                 rzp.open();
             };
         };
 
         loadRazorpay();
 
-        // Cleanup function to remove script on component unmount
         return () => {
             document.body.removeChild(document.querySelector('script[src="https://checkout.razorpay.com/v1/checkout.js"]'));
         };
